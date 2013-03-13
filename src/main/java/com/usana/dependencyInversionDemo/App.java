1d51c8ca2aaab2f169fd89b692a6c04aaa414c66
Monte Wingle <github@wingmantech.com>
2013-03-12 22:57:37 -0600

    First commit

---
+++ src/main/java/com/usana/dependencyInversionDemo/App.java
@@ -0,0 +1,13 @@
+package com.usana.dependencyInversionDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Dependency1.printHelloWorld();
+    }
+}
---
+++ src/main/java/com/usana/dependencyInversionDemo/Dependency3.java
@@ -0,0 +1,14 @@
+package com.usana.dependencyInversionDemo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: winglem
+ * Date: 3/12/13
+ * Time: 10:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dependency3 {
+    public static void printHelloWorld() {
+        System.out.println("Hello world.");
+    }
+}
